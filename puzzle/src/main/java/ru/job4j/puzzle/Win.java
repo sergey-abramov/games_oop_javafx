626d28ce7bf2d89c58adbc60060b2bae8138fd67
LESHA <belochuvach@gmail.com>
2022-07-17 22:37:47 +0300

    Сокобан. Тестовое задание.

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -3,6 +3,16 @@ package ru.job4j.puzzle;
 public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = true;
+        for (int i = 0; i < board.length - 1; i++) {
+            if (board[i][i] == 1) {
+                for (int j = 0; j < board.length - 1; j++) {
+                    if (board[i][j] != 1) {
+                        rsl = (board[j][i] == 1);
+                        break;
+                    }
+                }
+            }
+        }
         return rsl;
     }
 }
